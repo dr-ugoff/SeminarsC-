7b633d27c5f017c1b27675c7587ca23b9b442517
Andey Drugov <dr_ugoff@mail.ru>
2022-11-20 23:03:57 +0700

    add HomeWork 1

---
+++ HomeWorks/HW1/Program.cs
@@ -0,0 +1,82 @@
+﻿// ЗАДАЧА 1 
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5;  b = 7  -> max = 7
+// a = 2;  b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.WriteLine("Input number 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int b = Convert.ToInt32(Console.ReadLine());
+int min = a;
+int max = a;
+if (a > b)
+{
+    min = b;
+}
+else
+{
+    max = b;
+}
+Console.Write($"min = {min}, max = {max}");
+
+
+//<<------>>//
+
+
+// ЗАДАЧА 2
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Input number 1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number 2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number 3");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if (max < num2) max = num2;
+// if (max < num3) max = num3;
+// Console.WriteLine($"max = {max}");
+
+
+//<<------>>//
+
+
+// ЗАДАЧА 3
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4  -> да
+// -3 -> нет
+// 7  -> нет
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int even = num % 2;
+// if (even == 0) 
+// {
+//     Console.WriteLine($"{num} is even number -> yes");
+// }
+// else
+// {
+//     Console.WriteLine($"{num} is odd number -> no");
+// }
+
+
+//<<------>>//
+
+
+// ЗАДАЧА 4
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (count <= num)
+// {
+//     Console.WriteLine(count);
+//     count+=2;
+// }
---
+++ Seminars/seminar1/Program.cs
@@ -0,0 +1,76 @@
+﻿// Пример 1 (вывести число, которое ввел пользователь)
+
+// Console.WriteLine("Input number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(a);
+
+//<<------>>//
+
+
+// Пример 2 (вывести квадрат числа введенного )
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int square = num * num;
+// Console.WriteLine($"Square {num} is {square}");
+
+
+//<<------>>//
+
+
+// Пример 3 (Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго)
+// a = 25; b = 5  -> да
+// a = 2;  b = 10 -> нет
+// a = 9;  b = -3 -> да
+// a = -3; b = 9  -> нет
+
+// Console.WriteLine("Input number 1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number 2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int square = num2 * num2;
+// if (square == num1)
+// {
+//     Console.WriteLine($"{num1} is square of {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"{num1} is not square of {num2}");
+// }
+
+
+//<<------>>//
+
+
+// Задача 1 (Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N)
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = -1 * num;
+// while (count <= num)
+// {
+//     Console.WriteLine(count);
+//     count++;
+// }
+
+
+//<<------>>//
+
+
+// Задача 2 (Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа)
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8)
+
+Console.WriteLine("Input three-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+int lastdig = num ;
+if (lastdig < 0) lastdig = -1 * lastdig;
+Console.WriteLine($"The last digit is {lastdig}");
+
+// Альтернативный вариант решения из семинара в zoom
+// Console.WriteLine("Input three-digit number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Math.Abs(num) % 10);
