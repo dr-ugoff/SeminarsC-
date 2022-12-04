872d69de4acdcdd9a46113e229f2c02dbaf3d60a
Andey Drugov <dr_ugoff@mail.ru>
2022-12-04 18:29:50 +0700

    add seminar 7

---
+++ Seminars/seminar7/Program.cs
@@ -0,0 +1,213 @@
+﻿// ЗАДАЧА 1
+// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+// 1 2 3
+// 5 6 7
+// 8 9 0
+
+// int[,] Create2DRandomArray(int minValue, int maxValue, int rows, int columns)
+// {
+//     int[,] newArray = new int[rows, colums];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colums; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение элемента");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение элемента");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Show2DArray(Create2DRandomArray(min, max, rows, columns));
+
+
+
+// <<--------->>
+
+
+
+// ЗАДАЧА 2
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+// Выведите полученный массив на экран.
+
+// int[,] Create2DRandomArray(int rows, int colums)
+// {
+//     int[,] newArray = new int[rows, colums];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colums; j++)
+//         {
+//             newArray[i, j] = i + j;
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Show2DArray(Create2DRandomArray(rows, columns));
+
+
+
+// <<--------->>
+
+
+
+// ЗАДАЧА 3
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Новый массив будет выглядеть вот так:
+//1 4 7 2
+//5 **81 ** 2 **9**
+//8 4 2 4
+
+// int[,] Create2DRandomArray(int minValue, int maxValue, int rows, int columns)
+// {
+//     int[,] newArray = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] SqrEnenElements2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(i % 2 == 0 && j % 2 == 0) array[i, j] = array[i, j] * array[i, j];
+//         }
+//     }
+//     return array;
+// }
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение элемента");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение элемента");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int[,] myArray = Create2DRandomArray(min, max, rows, columns);
+// Console.WriteLine("Сгенерированный массив");
+// Show2DArray(myArray);
+// Console.WriteLine();
+// Console.WriteLine("Измененный массив");
+// Show2DArray(SqrEnenElements2DArray(myArray));
+
+
+
+// <<--------->>
+
+
+
+//ЗАДАЧА 4
+// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+//Например, задан массив:
+
+int[,] Create2DRandomArray(int minValue, int maxValue, int rows, int columns)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumDiagonalElements2DArray(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        // sum = sum + array[i, i]; // Решение группы на семинаре.
+        // Мое решение:
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j) sum = sum + array[i, j]; 
+        }
+    }
+    Console.Write($"Сумма элементов, находящихся на главной диагонали: {sum}");
+    return sum;
+}
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = Create2DRandomArray(min, max, rows, columns);
+Show2DArray(myArray);
+Console.WriteLine();
+SumDiagonalElements2DArray(myArray);
+
